bc141fc8a404b1a123b9a62c84f0bf1227bb4c4f
rafael <rafael.hazot@elbitsystems.com>
2024-07-12 15:01:25 +0300

    add-patients-list

--- ui-dev-app/src/Components/PatientsPanel/PatientsList/PatientsList.tsx
+++ ui-dev-app/src/Components/PatientsPanel/PatientsList/PatientsList.tsx
@@ -1,21 +1,24 @@
 import {Button, List, ListItem, ListItemText} from "@mui/material";
 import React from "react";
-import { PatientsListProps} from "../Patients.model";
+import {PatientsListProps} from "../Patients.model";
 
 
 export const PatientsList: React.FC<PatientsListProps> = ({patients, deletePatient}) => {
 
     return (
-        <List>
-            {patients.map((patient) => (
-                <ListItem key={patient.id}>
-                    <ListItemText
-                        primary={patient.patientName}
-                        secondary={`Age: ${patient.age} - Notes: ${patient.notes}`}
-                    />
-                    <Button onClick={() => deletePatient(patient.id)}>Delete</Button>
-                </ListItem>
-            ))}
-        </List>
+        <>
+            <span style={{margin: '25px', display: "flex", flexDirection: "column"}}>My Patients</span>
+            <List>
+                {patients.map((patient) => (
+                    <ListItem key={patient.id}>
+                        <ListItemText
+                            primary={patient.patientName}
+                            secondary={`Age: ${patient.age} - Notes: ${patient.notes}`}
+                        />
+                        <Button onClick={() => deletePatient(patient.id)}>Delete</Button>
+                    </ListItem>
+                ))}
+            </List>
+        </>
     );
 }
