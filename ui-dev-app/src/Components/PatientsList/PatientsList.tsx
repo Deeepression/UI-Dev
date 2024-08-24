b2030add960720bf1bbc22066d2819d2f734d922
rafael <rafael.hazot@elbitsystems.com>
2024-08-24 16:47:41 +0300

    maintaince

--- ui-dev-app/src/Components/PatientsList/PatientsList.tsx
+++ ui-dev-app/src/Components/PatientsList/PatientsList.tsx
@@ -1,15 +1,16 @@
-import {IconButton, List, ListItemButton, ListItemText} from "@mui/material";
-import React from "react";
-import {PatientsListProps} from "../PatientsPanel/Patients.model";
-import DeleteIcon from '@mui/icons-material/Delete';
-import {useNavigate} from 'react-router-dom';
-
+import { IconButton, List, ListItemButton, ListItemText } from '@mui/material'
+import React from 'react'
+import { PatientsListProps } from '../PatientsPanel/Patients.model'
+import DeleteIcon from '@mui/icons-material/Delete'
+import { useNavigate } from 'react-router-dom'
 
 export const PatientsList: React.FC<PatientsListProps> = ({patients, deletePatient}) => {
     const navigate = useNavigate();
 
     const handlePatientClick = (id: string) => {
         navigate(`/patient/${id}`);
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to top after navigating
+
     };
 
     return (
