9eccaf382844e8674631decf5b52b88f47677914
Yuvalzirin <yuvalzirin1997@cs.colman.ac.il>
2024-08-28 18:33:09 +0300

    logo

--- ui-dev-app/src/Components/NavBar/NavBar.tsx
+++ ui-dev-app/src/Components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link as RouterLink} from 'react-router-dom';
-import {AppBar, Button} from '@mui/material';
+import { AppBar, Button, Toolbar, Box } from '@mui/material';
 import {StyledToolBar} from "./NavBar.styles";
 
 
@@ -9,6 +9,9 @@ const Navbar: React.FC = () => {
     return (
         <AppBar position="static" color="default">
             <StyledToolBar>
+                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                 <img src={'finalprojectlogo.png'} alt="Logo" style={{ height: '45px', marginRight: '16px',borderRadius: '50%' }} />
+                </Box>
                 <Button component={RouterLink} to="/" color="inherit">Home</Button>
                 <Button component={RouterLink} to="/patient" color="inherit">My Patients</Button>
                 <Button component={RouterLink} to="/tracking" color="inherit">Tracking List</Button>
