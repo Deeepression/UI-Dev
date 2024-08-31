2396ca839136d060a3ea8545ad4a81bf2ed58107
rafael <rafael.hazot@elbitsystems.com>
2024-08-31 14:38:49 +0300

    update-list

--- ui-dev-app/src/Components/SettingScreen/Notifications.tsx
+++ ui-dev-app/src/Components/SettingScreen/Notifications.tsx
@@ -1,62 +1,112 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import Checkbox from '@mui/material/Checkbox';
-import Divider from '@mui/material/Divider';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Unstable_Grid2';
+import * as React from 'react'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  FormControlLabel,
+  FormGroup,
+  Stack,
+  Typography,
+  Grid,
+  Switch,
+  TextField
+} from '@mui/material'
 import { paperClasses } from '@mui/material'
+import { useState } from 'react'
 
-export function Notifications(): React.JSX.Element {
+export function Notifications (): React.JSX.Element {
+  const [email, setEmails] = useState({ value: 'example@example.com' })
+  const [phone, setPhones] = useState({ value: '+1234567890' })
+  const [isEditing, setIsEditing] = useState(false)
+
+  const handleChange = (type: string, value: string) => {
+    if (type === 'email') {
+      setEmails({ value })
+    } else if (type === 'phone') {
+      setPhones({ value })
+    }
+  }
+
   return (
     <form
       onSubmit={(event) => {
-        event.preventDefault();
+        event.preventDefault()
+        setIsEditing(false)
       }}
     >
-      <Card sx={{
-        borderRadius: '20px',
-        [`&.${paperClasses.elevation1}`]: {
-          boxShadow: '0 5px 22px 0 rgba(0, 0, 0, 0.04), 0 0 0 1px rgba(0, 0, 0, 0.06)',
-        },
-      }}>
-        <CardHeader subheader="Manage the notifications" title="Notifications" />
-        <Divider />
+      <Card
+        sx={{
+          borderRadius: '20px',
+          [`&.${paperClasses.elevation1}`]: {
+            boxShadow: '0 5px 22px 0 rgba(0, 0, 0, 0.04), 0 0 0 1px rgba(0, 0, 0, 0.06)',
+          },
+        }}
+      >
+        <CardHeader subheader="Manage the notifications" title="Notifications"/>
+        <Divider/>
         <CardContent>
-          <Grid container spacing={6} wrap="wrap">
+          <Grid container wrap="wrap">
             <Grid md={4} sm={6} xs={12}>
               <Stack spacing={1}>
                 <Typography variant="h6">Email</Typography>
+                {isEditing ? (
+                  <TextField
+                    style={{width: '90%'}}
+                    value={email.value}
+                    onChange={(e) => handleChange('email', e.target.value)}
+                  />
+                ) : (
+                  <Typography>{email.value}</Typography>
+                )}
                 <FormGroup>
-                  <FormControlLabel control={<Checkbox defaultChecked />} label="High Risk" />
-                  <FormControlLabel control={<Checkbox />} label="Medium Risk" />
-                  <FormControlLabel control={<Checkbox />} label="Low Risk" />
+                  <FormControlLabel control={<Switch defaultChecked/>} label="High Risk" disabled={!isEditing}/>
+                  <FormControlLabel control={<Switch/>} label="Medium Risk" disabled={!isEditing}/>
+                  <FormControlLabel control={<Switch/>} label="Low Risk" disabled={!isEditing}/>
                 </FormGroup>
               </Stack>
             </Grid>
             <Grid md={4} sm={6} xs={12}>
               <Stack spacing={1}>
                 <Typography variant="h6">Phone</Typography>
+                {isEditing ? (
+                  <TextField
+                    style={{width: '90%'}}
+                    value={phone.value}
+                    onChange={(e) => handleChange('phone', e.target.value)}
+                  />
+                ) : (
+                  <Typography>{phone.value}</Typography>
+                )}
                 <FormGroup>
-                  <FormControlLabel control={<Checkbox defaultChecked />} label="High Risk" />
-                  <FormControlLabel control={<Checkbox />} label="Medium Risk" />
-                  <FormControlLabel control={<Checkbox />} label="Low Risk" />
+                  <FormControlLabel control={<Switch defaultChecked/>} label="High Risk" disabled={!isEditing}/>
+                  <FormControlLabel control={<Switch/>} label="Medium Risk" disabled={!isEditing}/>
+                  <FormControlLabel control={<Switch/>} label="Low Risk" disabled={!isEditing}/>
                 </FormGroup>
               </Stack>
             </Grid>
           </Grid>
         </CardContent>
-        <Divider />
+        <Divider/>
         <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <Button sx={{ borderRadius: '12px', textTransform: 'none' }} variant="contained">Save changes</Button>
+          <Button
+            sx={{ borderRadius: '12px', textTransform: 'none' }}
+            variant="outlined"
+            onClick={() => setIsEditing((prev) => !prev)}
+          >
+            {isEditing ? 'Discard' : 'Edit'}
+          </Button>
+          <Button
+            sx={{ borderRadius: '12px', textTransform: 'none' }}
+            variant="contained"
+            type="submit"
+          >
+            Save changes
+          </Button>
         </CardActions>
       </Card>
     </form>
-  );
+  )
 }
